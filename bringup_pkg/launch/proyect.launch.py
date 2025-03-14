2dc052926414c77bc343e447cc41c28e6f77b673
jorge <j.avilaorero@gmail.com>
2025-03-14 21:03:23 +0100

    Fichero launch en bringup_pkg, que lanza correctamente el simulador de Gazebo del paquete description_pkg, este launch incluye tambien el lanzador de ROS y del bridge entre gazebo y ROS, pero esta desactivado porque aún no está implementado

---
+++ bringup_pkg/launch/proyect.launch.py
@@ -0,0 +1,40 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+import os
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    # Rutas a los paquetes
+    description_pkg = get_package_share_directory('description_pkg')
+    control_pkg = get_package_share_directory('control_pkg')
+
+    # Lanzar Gazebo con el robot en SDF
+    gazebo_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(description_pkg, 'launch', 'sim_sdf.launch.py')
+        )
+    )
+
+    # Lanzar el puente de comunicación entre ROS 2 y Gazebo (ros_gz_bridge)
+    bridge_node = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=['/cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist'],
+        output='screen'
+    )
+
+    # Lanzar el nodo de control en C++
+    control_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(control_pkg, 'launch', 'control.launch.py')
+        )
+    )
+
+    return LaunchDescription([
+        
+        #bridge_node,
+        #control_launch,
+        gazebo_launch
+    ])
